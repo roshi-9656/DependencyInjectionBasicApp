9c0b65cd912528a43b934eba1b1d83c18395a3a2
roshi-9656 <roshin.rajendran@gmail.com>
2023-03-27 16:29:23 +0530

    Creating single instance for dependency injection methods

--- app/src/main/java/com/arclightcreatives/di_basicapparchitecture/di/Injection.kt
+++ app/src/main/java/com/arclightcreatives/di_basicapparchitecture/di/Injection.kt
@@ -6,8 +6,23 @@ import com.arclightcreatives.di_basicapparchitecture.utils.NetManager
 import com.arclightcreatives.di_basicapparchitecture.viewmodels.MainViewModelFactory
 
 object Injection {
-    fun provideMainviewmodelfactory(context: Context) : MainViewModelFactory{
-            val repository = Repository(NetManager(context))
-            return MainViewModelFactory(repository)
+
+    private var netManager: NetManager? = null
+
+    fun providesNetmanager(context: Context): NetManager {
+        if (netManager == null) {
+            netManager = NetManager(context)
+        }
+        return netManager!!
+    }
+
+    fun provideRepository(netManager: NetManager): Repository {
+        return Repository(netManager)
+    }
+
+    fun provideMainviewmodelfactory(context: Context): MainViewModelFactory {
+        val networkmanager = providesNetmanager(context)
+        val repository = provideRepository(networkmanager)
+        return MainViewModelFactory(repository)
     }
 }
